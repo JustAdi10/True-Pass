bd50fadbdcd26d8fcfcca09ff1ea7c292cba9560
Aditya Pawar <aditya.pawar23624@gmail.com>
2025-03-23 18:15:25 +0530

    commmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
@@ -11,12 +11,33 @@ import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 import CartPage from "./pages/CartPage";
 import RoleSelection from './pages/RoleSelection';
-import AttendeeLogin from './pages/AttendeeLogin'; // Import AttendeeLogin
-import OrganizerLogin from './pages/OrganizerLogin'; // Import OrganizerLogin
+import AttendeeLogin from './pages/AttendeeLogin';
+import OrganizerLogin from './pages/OrganizerLogin';
+
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === "/"; // Hide Header & Footer only on RoleSelection
+
+  return (
+    <>
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/" element={<RoleSelection />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/category/:categoryName" element={<CategoryPage />} />
+        <Route path="/event/:eventId" element={<EventDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/attendee/login" element={<AttendeeLogin />} />
+        <Route path="/organizer/login" element={<OrganizerLogin />} />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
   useEffect(() => {
-    // Function to inject a script dynamically
     const loadScript = (src) => {
       if (!document.querySelector(`script[src="${src}"]`)) {
         const script = document.createElement("script");
@@ -26,27 +47,15 @@ function App() {
       }
     };
 
-    // Inject BotPress scripts
     loadScript("https://cdn.botpress.cloud/webchat/v2.2/inject.js");
     loadScript("https://files.bpcontent.cloud/2025/03/22/16/20250322163818-GEG8L1FS.js");
-  }, []); // Runs only once when the component mounts
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<RoleSelection />} /> {/* Set RoleSelection as the default route */}
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/category/:categoryName" element={<CategoryPage />} />
-          <Route path="/event/:eventId" element={<EventDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/attendee/login" element={<AttendeeLogin />} /> {/* Attendee Login route */}
-          <Route path="/organizer/login" element={<OrganizerLogin />} /> {/* Organizer Login route */}
-        </Routes>
-        <Footer />
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
