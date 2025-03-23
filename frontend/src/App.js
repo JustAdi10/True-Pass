60d1e6157a0fcb8427c3a774362ac7d220ec35bf
Aditya Pawar <aditya.pawar23624@gmail.com>
2025-03-23 13:38:55 +0530

    cyka blyat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,8 @@ import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 import CartPage from "./pages/CartPage";
 import RoleSelection from './pages/RoleSelection';
-import AttendeeLogin from './pages/AttendeeLogin'; // Import AttendeeLogin
-import OrganizerLogin from './pages/OrganizerLogin'; // Import OrganizerLogin
+import AttendeeLogin from './pages/AttendeeLogin';
+import OrganizerLogin from './pages/OrganizerLogin';
 
 function App() {
   useEffect(() => {
@@ -37,14 +37,14 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<HomePage />} /> {/* Default route */}
+          <Route path="/" element={<RoleSelection />} /> {/* Set RoleSelection as the default route */}
           <Route path="/home" element={<HomePage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/category/:categoryName" element={<CategoryPage />} />
           <Route path="/event/:eventId" element={<EventDetailPage />} />
           <Route path="/cart" element={<CartPage />} />
-          <Route path="/attendee/login" element={<AttendeeLogin />} /> {/* Attendee Login route */}
-          <Route path="/organizer/login" element={<OrganizerLogin />} /> {/* Organizer Login route */}
+          <Route path="/attendee/login" element={<AttendeeLogin />} />
+          <Route path="/organizer/login" element={<OrganizerLogin />} />
         </Routes>
         <Footer />
       </Router>
